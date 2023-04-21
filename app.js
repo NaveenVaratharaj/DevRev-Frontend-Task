33bb35da519cba3b7de65843429e6dbd90ca8deb
NaveenVaratharaj <naveenvm.cs20@bitsathy.ac.in>
2023-04-21 15:36:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 require('dotenv').config(); //DOTENV for security
 const ejs = require("ejs");
 const express = require("express");
+const path = require('path');
 
 const app = express();
 
 //View engine setup and serving static files
-app.set('view engine', ejs);
+// app.set('view engine', ejs);
 app.use(express.static("public"));
 
 app.get("/", (req,res) =>{
-    res.render("home.ejs");
+    res.sendFile(path.join(__dirname+'/files/index.html'))
+})
+
+app.get("/findbooks", (req,res) =>{
+    res.sendFile(path.join(__dirname+'/files/main.html'))
 })
 
 app.listen(process.env.PORT_NUMBER, (req,res) =>{
