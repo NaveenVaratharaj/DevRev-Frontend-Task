730182ae18d254bdb62cce7a4c15e9775c28c889
Naveen Varatharaj <88475979+NaveenVaratharaj@users.noreply.github.com>
2023-04-22 08:48:05 +0530

    Routes for Login and SignUp

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ app.get("/findbooks", (req,res) =>{
     res.sendFile(path.join(__dirname+'/files/main.html'))
 })
 
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/files/login.html'))
+})
+
+app.get('/signup', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/files/signup.html'))
+})
+
 app.listen(4000, (req,res) =>{
     console.log(`App started at the port 4000` )
 })
